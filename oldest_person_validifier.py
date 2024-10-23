0db2f9477ed60b4955422c2243fb272ad54926a9
JD SOON <jdangelogsoon@gmail.com>
2024-10-23 23:22:00 +0800

    Established main variable

--- oldest_person_validifier.py
+++ oldest_person_validifier.py
@@ -1,16 +1,51 @@
- Create a program that ask user to input name and age. Print error message when the input is not valid. Set your own definition of valid name and valid age. 
- Store all the collected information into array. After every input, ask the user if want to input another entry. 
- When “Yes”, ask the user again for input. Do it until the user respond “No”. When the user responded “No”, display the name and age of the oldest person.
- Use the array in checking who is the oldest.
+oldest_person_validifier
 
-set main variable name 
-loop 1: 
-ask user to input name
-    if integer, print error
-ask user to input age
-    if letters, print error
-    if greater than 110, print error
-    if successful, put user inputs into an array
-        ask user if they want to input another entry
-            if yes, return loop 1
-            if no, print name and age of the oldest person
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Create a program that ask user to input name and age. Print error message when the input is not valid. Set your own definition of valid name and valid age. 
+ #Store all the collected information into array. After every input, ask the user if want to input another entry. 
+ #hen “Yes”, ask the user again for input. Do it until the user respond “No”. When the user responded “No”, display the name and age of the oldest person.
+ #Use the array in checking who is the oldest.
+
+#set main variable name 
+#loop 1: 
+#ask user to input name
+#    if integer, print error
+#ask user to input age
+#    if letters, print error
+#    if greater than 110, print error
+#    if successful, put user inputs into an array
+#        ask user if they want to input another entry
+#            if yes, return loop 1
+#            if no, print name and age of the oldest person
