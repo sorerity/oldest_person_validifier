6118a106253e49281da3fe25bc42245c008d2f55
JD SOON <jdangelogsoon@gmail.com>
2024-10-24 23:04:11 +0800

    Fixed indentations and alignments which causes certain variables to be undefined. Also fixed the function which recognizes the oldest person on the list.

--- oldest_person_validifier.py
+++ oldest_person_validifier.py
@@ -17,8 +17,8 @@ while True:
 
             list.append(oldest_person_validifier)
 
-            print(oldest_person_validifier["Name"])
-            print(oldest_person_validifier["Age"])
+            print(f"Name: {oldest_person_validifier['Name']}")
+            print(f"Age: {oldest_person_validifier['Age']}")
 
             retry = input("Do you want to input another entry? Y/N:  ")
             break
@@ -26,16 +26,20 @@ while True:
             print("Error.")
             
     if retry == "N":
+        if list:
+            oldest_person = list[0]
+
+            for person in list:
+                if person["Age"] > oldest_person["Age"]:
+                    oldest_person = person
+
             print(f"The oldest person is {oldest_person['Name']} with the age of {oldest_person['Age']}.")
+        else:
+            print("No valid entries were made.")
+        break
+
     elif retry != "Y":
         print("Invalid")
-
-    if list:
-        oldest_person = list[0]
-    
-    for person in list:
-        if person["Age"] > oldest_person["Age"]:
-            oldest_person = person
 
 
 
